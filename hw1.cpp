031c868b8c82c75435f3b4d16d995cde63b9ff74
dbseend <141024382+dbseend@users.noreply.github.com>
2023-09-05 00:18:57 +0900

    Add files via upload
    hw1

---
+++ hw1.cpp
@@ -0,0 +1,37 @@
+#include "opencv.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+    Mat frame;
+    VideoCapture cap;
+    int fps;
+    int delay;
+    int current_frame;
+    int total_frame;
+
+    if (cap.open("background.mp4") == 0)
+    {
+        cout << "no such file!" << endl;
+        waitKey(0);
+    }
+
+    fps = cap.get(CAP_PROP_FPS);
+    delay = 1000 / fps;
+    total_frame = cap.get(CAP_PROP_FRAME_COUNT); // 전체 프레임 수
+
+    while (cap.get(CAP_PROP_POS_MSEC) <= 3000)
+    {
+        cap >> frame;
+        current_frame = cap.get(CAP_PROP_POS_FRAMES);
+        imshow("video", frame);
+        waitKey(delay);
+
+        cout << "frames: " << current_frame << " / " << total_frame << endl;
+    }
+
+    return 0; 
+}
